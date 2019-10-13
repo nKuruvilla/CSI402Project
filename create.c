dbe7ffe1173003cc557fc18081a07b03abad0ac4
Avinash M Singh <atomeundwolke@gmail.com>
2019-10-13 03:03:45 -0400

    Updated create.c

--- create.c
+++ create.c
@@ -3,7 +3,7 @@
 #include <string.h>
 #include <unistd.h>
 
- int create(int argc, char *argv[]){
+ int create(FILE *file, FILE *old, int argc, char *argv[]){
  
  /*Create will be executed by naivedb, or from the terminal by running its executable file */ 
      
@@ -11,14 +11,14 @@ if (argc == 1 || argc > 4){
     printf("Please input a proper command.\n");
     exit(0);
 }else if (argc == 3){
-    if (strcmp(argv[1], '-f') == 0){
+    if (strcmp(argv[1], "-f") == 0){
  /*Here is where we will place the code for filepath:
  *      create -f filepath
  *          ~ creates an empty, ordinary file whose name is given in the specified path.
  *            The path can be an absolute, relative, or a bare filename. The new file should
  *            have permission 0640 specified in octal.
  */
-    }else if (strcmp(argv[1], '-d') == 0){
+    }else if (strcmp(argv[1], "-d") == 0){
  /*Here is where we will place the code for dirpath:
  *      create -d dirpath
  *          ~ creates a new directory whose name is given in the specified path. New file should  
@@ -31,7 +31,7 @@ if (argc == 1 || argc > 4){
  *            is the path or name of the hard link created.
  */
     
-    if (strcmp(argv[1], '-h') == 0){
+    if (strcmp(argv[1], "-h") == 0){
         link (argv[2], argv[3]);
         return 0;
     }
@@ -40,7 +40,7 @@ if (argc == 1 || argc > 4){
  *          ~ creates a soft/sym link. 'Oldname' is the path or name of an existing file, an
  *            linkname is the path or name of the soft link created.
  */
-    else if (strcmp(argv[1], '-s') == 0){
+    else if (strcmp(argv[1], "-s") == 0){
         symlink (argv[2], argv[3]);
         return 0;
     }
